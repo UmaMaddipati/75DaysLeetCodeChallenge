39923ab8e042cb111baa2fced338cdc9c03cd63d
UmaMaddipati <158579245+UmaMaddipati@users.noreply.github.com>
2026-03-30 15:44:37 +0530

    Time: 0 ms (100%), Space: 42.4 MB (96.19%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,48 @@
+class MyQueue {
+     
+     Stack<Integer> s1,s2;
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+        
+    }
+    
+    public int pop() {
+        
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            
+        }
+        if(s2.isEmpty()){
+            return -1;
+        }
+
+        int topelement = s2.pop();
+        return topelement;
+    }
+    
+    public int peek() {
+
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+            s2.push(s1.pop());
+            }
+
+        }
+
+        int topelement = s2.peek();
+        return topelement;
+        
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+        
+    }
+}
